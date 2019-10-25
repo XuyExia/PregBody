1a7dd87d0879421e0b242b2aa2168996b31ee7fb
xuyifan <1135672070@qq.com>
2019-10-25 15:15:11 +0800

    sss

---
+++ src/main/java/com/xuyifan/pregbody/PregbodyApplication.java
@@ -0,0 +1,13 @@
+package com.xuyifan.pregbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PregbodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PregbodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuyifan/pregbody/PregbodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuyifan.pregbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PregbodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
